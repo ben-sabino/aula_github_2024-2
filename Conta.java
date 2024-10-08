cfd6de7e58a5a9488ea81215eae62f1ab972e131
Caio Burton <101891262+CaioBurton@users.noreply.github.com>
2024-10-07 21:53:03 -0300

    consultar saldo

---
+++ Conta.java
@@ -0,0 +1,25 @@
+class Conta {
+    private double saldo;
+
+    public Conta(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    // Método para consultar o saldo
+    public double consultarSaldo() {
+        return saldo;
+    }
+
+    // Método para depositar
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    // Método para sacar
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
